7d1ec9e964c96f9dcbbb6e3658631fe9eff39105
Cosmitar <mhherrera31@gmail.com>
2024-02-27 13:35:31 +0100

    refactor: endpoints and ui for posts by id and by user

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,8 +1,14 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import PostView from "~/components/PostView/PostView";
+import { api } from "~/utils/api";
 
 const Post = () => {
   const router = useRouter();
+
+  const { data, isLoading: postsLoading } = api.post.getById.useQuery({
+    postId: router.query.id as string,
+  });
   return (
     <>
       <Head>
@@ -11,6 +17,9 @@ const Post = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           POST BY ID: {router.query.id}
+          {data?.map((postAndAuthor) => (
+            <PostView key={postAndAuthor.post.id} {...postAndAuthor} />
+          ))}
         </div>
       </main>
     </>
