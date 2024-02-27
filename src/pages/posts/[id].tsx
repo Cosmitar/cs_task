7cdb5d5289416fd8e512b3605bf7995acc2cadb5
Cosmitar <mhherrera31@gmail.com>
2024-02-27 22:56:13 +0100

    refacto: add comment list under post view

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
+import CommentForm from "~/components/CommentForm/CommentForm";
+import CommentsList from "~/components/CommentsList/CommentsList";
 import PostView from "~/components/PostView/PostView";
 import { api } from "~/utils/api";
 
@@ -9,7 +12,8 @@ const Post = () => {
   const { data, isLoading: postsLoading } = api.post.getById.useQuery({
     postId: router.query.id as string,
   });
-  
+  const fullPostInfo = data ? data[0] : null;
+
   return (
     <>
       <Head>
@@ -18,9 +22,13 @@ const Post = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           POST BY ID: {router.query.id}
-          {data?.map((postAndAuthor) => (
-            <PostView key={postAndAuthor.post.id} {...postAndAuthor} />
-          ))}
+          {fullPostInfo && (
+            <Fragment key={fullPostInfo.post.id}>
+              <PostView {...fullPostInfo} />
+              <CommentForm commentPath={fullPostInfo.post.id} />
+              <CommentsList commentPath={fullPostInfo.post.id} />
+            </Fragment>
+          )}
         </div>
       </main>
     </>
