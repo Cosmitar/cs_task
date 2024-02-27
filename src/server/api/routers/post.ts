12c68a49549282fa4a62f63e874284f4d569ab9a
Cosmitar <mhherrera31@gmail.com>
2024-02-27 13:08:39 +0100

    refactor: protect creation requests with rate limit

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -6,6 +6,22 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
+
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
@@ -27,6 +43,9 @@ export const postRouter = createTRPCRouter({
       // await new Promise((resolve) => setTimeout(resolve, 1000));
 
       const authorId = ctx.userId!;
+
+      const { success } = await ratelimit.limit(authorId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
       return ctx.db.post.create({
         data: {
