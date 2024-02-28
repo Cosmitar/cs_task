702509ee7a7c22c02b9d3854a0f1977a024a6abf
Cosmitar <mhherrera31@gmail.com>
2024-02-28 03:58:25 +0100

    style: comment list and some assorted fixes

--- src/components/SVG/DownvoteArrow.tsx
+++ src/components/SVG/DownvoteArrow.tsx
@@ -1,8 +1,8 @@
 export default function DownvoteArrow() {
   return (
     <svg
-      width="16"
-      height="11"
+      width="100%"
+      height="100%"
       viewBox="0 0 16 11"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
