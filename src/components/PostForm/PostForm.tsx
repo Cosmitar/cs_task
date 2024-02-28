dfde72426b673fd5858befeca4467d9523088c64
Cosmitar <mhherrera31@gmail.com>
2024-02-28 02:22:01 +0100

    style: post form

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { api } from "~/utils/api";
@@ -10,8 +11,8 @@ export function PostForm() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  const user = useUser();
-  if (!user) return <></>;
+  const auth = useUser();
+  if (!auth) return <></>;
 
   const createPost = api.post.create.useMutation({
     onSuccess: () => {
@@ -27,29 +28,44 @@ export function PostForm() {
         e.preventDefault();
         createPost.mutate({ title, content });
       }}
-      className="flex flex-col gap-2"
+      className="post-form my-10 flex w-full rounded-xl border border-gray-200 p-4 pb-3"
     >
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className="w-full rounded-full px-4 py-2 text-black"
-      />
-      <input
-        type="text"
-        placeholder="Content"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        className="w-full rounded-full px-4 py-2 text-black"
-      />
-      <button
-        type="submit"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-        disabled={createPost.isLoading}
-      >
-        {createPost.isLoading ? "Submitting..." : "Submit"}
-      </button>
+      <div>
+        <Image
+          src={auth?.user?.imageUrl ?? ""}
+          className="mr-4 h-6 w-6 rounded-full"
+          alt={`${auth?.user?.firstName}'s profile picture`}
+          width={24}
+          height={24}
+        />
+      </div>
+      <div className="w-full">
+        <input
+          type="text"
+          placeholder="Title of your post"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full px-0 py-0 text-black outline-none "
+        />
+        <input
+          type="text"
+          placeholder="Share your thoughts with the world!"
+          multiple
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="w-full px-0 py-2 text-black outline-none"
+        />
+        <div className="w-full border-b border-gray-200" />
+        <div className="mt-4 flex w-full items-center justify-end">
+          <button
+            type="submit"
+            className="primary-button font-semibold transition hover:bg-indigo-500"
+            disabled={createPost.isLoading}
+          >
+            {createPost.isLoading ? "Posting..." : "Post"}
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
