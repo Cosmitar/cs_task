f8f07d8d6576aaa61595ea73729708995769a5f3
Cosmitar <mhherrera31@gmail.com>
2024-02-28 04:09:12 +0100

    style: show posts by user

--- src/pages/posts/user/[slug].tsx
+++ src/pages/posts/user/[slug].tsx
@@ -1,30 +1,42 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { PostForm } from "~/components/PostForm/PostForm";
 import PostView from "~/components/PostView/PostView";
+import Sidebar from "~/components/Sidebar/Sidebar";
 import { api } from "~/utils/api";
 
 const User = () => {
   const router = useRouter();
-
-  const { data, isLoading: postsLoading } = api.post.getByUser.useQuery({
-    authorId: router.query.slug as string,
-  });
+  const authorId = router.query.slug as string;
 
   return (
     <>
       <Head>
         <title>CS - User Posts</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          USER POSTS: {router.query.slug}
-          {data?.map((postAndAuthor) => (
-            <PostView key={postAndAuthor.post.id} {...postAndAuthor} />
-          ))}
-        </div>
+      <Sidebar />
+      <main className="main-container">
+        {authorId && <PostsListByUser authorId={authorId} />}
       </main>
     </>
   );
 };
 
 export default User;
+
+const PostsListByUser = ({ authorId }: { authorId: string }) => {
+  const { data } = api.post.getByUser.useQuery({
+    authorId,
+  });
+
+  return (
+    <>
+      <PostForm />
+      <div className="w-full  [&>*:first-child]:mt-0 [&>*]:mt-10">
+        {data?.map((postAndAuthor) => (
+          <PostView key={postAndAuthor.post.id} {...postAndAuthor} />
+        ))}
+      </div>
+    </>
+  );
+};
