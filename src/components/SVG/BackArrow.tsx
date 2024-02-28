0acb51bbfe07dd864acf5a56ac8c812dcc48761c
Cosmitar <mhherrera31@gmail.com>
2024-02-28 02:22:20 +0100

    refactor: move svg into components

---
+++ src/components/SVG/BackArrow.tsx
@@ -0,0 +1,19 @@
+export default function BackArrow() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.33333 4.16667L2.5 9.99995M2.5 9.99995L8.33333 15.8333M2.5 9.99995H17.5"
+        stroke="#1F2937"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/components/SVG/UpvoteArrow.tsx
@@ -0,0 +1,19 @@
+export default function UpvoteArrow() {
+  return (
+    <svg
+      width="16"
+      height="11"
+      viewBox="0 0 16 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.3335 7.6667L8.00016 1.00003L14.6668 7.6667M4.66683 10.1665L8.00016 6.83321L11.3335 10.1665"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
